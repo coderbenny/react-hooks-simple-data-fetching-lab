90e931e00ae7b5b9b3d144798821e15f5d1f0bfe
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-11 16:14:12 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
+import React, { useState, useEffect } from "react"
+
 // create your App component here
+function App() {
+
+    // State for API Data
+    const [dogData, setDogData] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
+
+    // useEffect Hook for fetching Image
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(data => {
+                setDogData(data)
+                setIsLoading(false)
+            })
+    }, [])
+
+    if (isLoading) {
+        return (<p>Loading...</p>)
+    }
+
+    return (
+        <img src={dogData.message} alt="A Random Dog" />
+    )
+}
+
+
+export default App;
